05e46fd56d1f011fae36f95d7797f7cfdf651846
moon1324 <michaelshouldhope@gmail.com>
2024-02-13 21:51:36 +0900

    feat: add g_object files

---
+++ js/workspace/g_object/b_object.js
@@ -0,0 +1,22 @@
+// let dinner = {}랑 똑같다
+// 다만, 나중에 추가하겠다는 의미
+// new: heap 메모리에 할당
+// Object(): 객체의 프로퍼티가 들어갈 수 있는 공간을 확보하고 그 주소를 가져와준다.
+
+let dinner = new Object();
+let user = {
+    money: 10000,
+};
+
+dinner.name = "pizza";
+dinner.price = "30000";
+// 주소를 전달받은 뒤,
+dinner.pay = (user) => {
+    // 직접 주소에 접근해서 값을 수정한다.
+    user.money -= dinner.price;
+};
+
+// user 객체가 가지고 있는 주소를 전달한다.
+dinner.pay(user);
+// 원본 객체인 user의 money는 이미 수정되어있다.
+console.log(user.money);
