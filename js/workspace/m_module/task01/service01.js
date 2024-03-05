956f5c3146eafb718e06e77d929a11591b6a8746
moon1324 <michaelshouldhope@gmail.com>
2024-03-05 16:07:29 +0900

    feat: update l_date folder, m_module folder

---
+++ js/workspace/m_module/task01/service01.js
@@ -0,0 +1,33 @@
+const userService = (() => {
+    // const findAll = async (callback) => {
+    //     const response = await fetch(
+    //         "https://jsonplaceholder.typicode.com/users/"
+    //     );
+    //     const users = await response.json();
+    //     if (callback) {
+    //         callback(users);
+    //     }
+
+    // };
+    const findAll = async (callback) => {
+        const response = await fetch(
+            "https://jsonplaceholder.typicode.com/users/"
+        );
+        const users = await response.json();
+        if (callback) {
+            callback(users);
+        }
+    };
+
+    const findById = async (id, callback) => {
+        const response = await fetch(
+            `https://jsonplaceholder.typicode.com/users/${id}`
+        );
+        const user = await response.json();
+        if (callback) {
+            callback(user);
+        }
+    };
+
+    return { findAll: findAll, findById: findById };
+})();
