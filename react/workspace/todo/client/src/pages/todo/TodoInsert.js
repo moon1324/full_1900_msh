aadc0b6a8fda8e821bdd096e03968e843db7dd87
moon1324 <michaelshouldhope@gmail.com>
2024-05-20 20:16:02 +0900

    fix: replace ENTER as Enter in if loof of TodoInsert.js

--- react/workspace/todo/client/src/pages/todo/TodoInsert.js
+++ react/workspace/todo/client/src/pages/todo/TodoInsert.js
@@ -9,8 +9,8 @@ const TodoInsert = ({ todos, setIsTodoUpdate, isTodoUpdate }) => {
     // 투두리스트 추가, POST
     // enter를 쳤을 때 confirm 창을 띄워서 이대로 추가하시겠습니까? 로직을 만들기
     const onKeyDownAddTodo = async (e) => {
-        // console.log(e);
-        if (e.key === "ENTER") {
+        console.log(e);
+        if (e.key === "Enter") {
             console.log("사용자가 엔터를 누름");
             // console.log(value);
             if (!window.confirm("이대로 추가하시겠습니까?")) return;
